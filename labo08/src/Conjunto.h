e49c671cd24f210ad1da50d23e3020fdc35acce5
fbigiolli <felipebigiolli@gmail.com>
2023-05-08 21:50:12 -0300

    labo08 initial commit

---
+++ labo08/src/Conjunto.h
@@ -0,0 +1,78 @@
+#ifndef CONJUNTO_H_
+#define CONJUNTO_H_
+
+#include <assert.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class Conjunto
+{
+    public:
+
+        // Constructor. Genera un conjunto vacío.
+        Conjunto();
+
+        // Destructor. Debe dejar limpia la memoria.
+        ~Conjunto();
+
+        // Inserta un elemento en el conjunto. Si este ya existe,
+        // el conjunto no se modifica.
+        void insertar(const T&);
+
+        // Decide si un elemento pertenece al conjunto o no.
+        bool pertenece(const T&) const;
+
+        // Borra un elemento del conjunto. Si este no existe,
+        // el conjunto no se modifica.
+        void remover(const T&);
+
+        // Siguiente elemento al recibido por párametro, en orden.
+        const T& siguiente(const T& elem);
+
+        // Devuelve el mínimo elemento del conjunto según <.
+        const T& minimo() const;
+
+        // Devuelve el máximo elemento del conjunto según <.
+        const T& maximo() const;
+
+        // Devuelve la cantidad de elementos que tiene el conjunto.
+        unsigned int cardinal() const;
+
+        // Muestra el conjunto.
+        void mostrar(std::ostream&) const;
+
+    private:
+
+        /**
+         * Completar con lo que sea necesario...
+         **/
+
+        struct Nodo
+        {
+            // El constructor, toma el elemento al que representa el nodo.
+            Nodo(const T& v);
+            // El elemento al que representa el nodo.
+            T valor;
+            // Puntero a la raíz del subárbol izquierdo.
+            Nodo* izq;
+            // Puntero a la raíz del subárbol derecho.
+            Nodo* der; 
+        };
+
+        // Puntero a la raíz de nuestro árbol.
+        Nodo* _raiz;
+
+};
+
+template<class T>
+std::ostream& operator<<(std::ostream& os, const Conjunto<T>& c) {
+	 c.mostrar(os);
+	 return os;
+}
+
+#include "Conjunto.hpp"
+
+#endif // CONJUNTO_H_
---
+++ labo08/src/Conjunto.hpp
@@ -0,0 +1,53 @@
+
+template <class T>
+Conjunto<T>::Conjunto() {
+    // Completar
+}
+
+template <class T>
+Conjunto<T>::~Conjunto() { 
+    // Completar
+}
+
+template <class T>
+bool Conjunto<T>::pertenece(const T& clave) const {
+    assert(false);
+    return false;
+}
+
+template <class T>
+void Conjunto<T>::insertar(const T& clave) {
+    assert(false);
+}
+
+template <class T>
+void Conjunto<T>::remover(const T&) {
+    assert(false);
+}
+
+template <class T>
+const T& Conjunto<T>::siguiente(const T& clave) {
+    assert(false);
+}
+
+template <class T>
+const T& Conjunto<T>::minimo() const {
+    assert(false);
+}
+
+template <class T>
+const T& Conjunto<T>::maximo() const {
+    assert(false);
+}
+
+template <class T>
+unsigned int Conjunto<T>::cardinal() const {
+    assert(false);
+    return 0;
+}
+
+template <class T>
+void Conjunto<T>::mostrar(std::ostream&) const {
+    assert(false);
+}
+
